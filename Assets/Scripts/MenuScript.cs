01f29e6e4e8b7dc54cb0d984cdfa8f6a38908246
Anson brown <anson9@gmail.com>
2021-11-11 21:04:07 -0600

    Added more scripts. Camera follow, etc

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public Settings settings;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Music music = Object.FindObjectOfType<Music>();
+        settings = Object.FindObjectOfType<Settings>();
+
+        if (music)
+        {
+            //music.playMainMenuMusic();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+
+    public void playGame()
+    {
+        SceneManager.LoadScene("main");
+    }
+}
